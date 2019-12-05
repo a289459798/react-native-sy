b94c4e1e391280c2054603448e8f46d8e053f436
zhangzy <zhangzy0816@gmail.com>
2019-12-05 16:47:18 +0800

    [新增功能](1.0.0): 集成闪验
    Closes:

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
+import {NativeModules} from 'react-native';
 
-import { NativeModules } from 'react-native';
+const {RNSy, NativeEventEmitter} = NativeModules;
 
-const { RNSy } = NativeModules;
+class Sy extends NativeEventEmitter {
+    // 构造
+    constructor(props) {
+        super(RNSy);
+        // 初始状态
+        this.state = {};
+    }
 
-export default RNSy;
+    init(appid, debug, cb) {
+        RNSy.login(appid, debug, cb);
+    }
+
+    preGetPhonenumber(cb) {
+        RNSy.preGetPhonenumber(cb);
+    }
+
+    login(cb) {
+        RNSy.login(cb);
+    }
+}
+
+let Sy = new Sy();
+export default Sy;
